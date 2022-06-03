b064afd314baab78ec002626a9a13c4260672362
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-03 20:46:14 +0530

    Update channel.py

--- Doctor_Strange/channel.py
+++ Doctor_Strange/channel.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from info import CHANNELS
-from database.ia_filterdb import save_file
+from Cluster.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video | filters.audio
 
