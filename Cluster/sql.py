941a33d6a66fe22d2a5a7be82c96be6b2d61e6fa
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-06-04 17:21:53 +0530

    Update sql.py

--- Cluster/sql.py
+++ Cluster/sql.py
@@ -1,25 +1,12 @@
+import pymongo 
 import os
-from urllib.parse import urljoin, urlparse
-from enum import unique
-from typing import Type
 
-from pyrogram.session.internals import data_center
-from sqlalchemy import JSON as js
-from sqlalchemy import (BigInteger, Boolean, Column, Integer, MetaData, String,
-                        Table, create_engine)
-from sqlalchemy.sql.expression import column
-from sqlalchemy.sql.sqltypes import PickleType
-
-database_url = urlparse(database_url)._replace(scheme="postgresql").geturl()
-engine = create_engine(database_url, echo=False)
-
+DB_NAME = os.environ.get("DB_NAME","")
+DB_URL = os.environ.get("DB_URL","")
+mongo = pymongo.MongoClient(DB_URL)
+db = mongo[DB_NAME]
+dbcol = db["user"]
 
 def setWelcome(group_id, welcome):
-    try:
-        with engine.connect() as conn:
-            groups.add(group_id)
-            return(conn.execute(groupsTable.update(None).where(
-                groupsTable.c.groupid == group_id).values(welcome=welcome)))
-    except Exception as E:
-        print(E)
-        return False
+    dbcol.update_one({"_id": group_id},{"$set":{"welcome": welcome}})
+	
