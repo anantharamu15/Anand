51b62bd51f4f867ece3163b8d8494bf2afb86995
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-04 13:13:46 +0530

    Update commands.py

--- Doctor_Strange/commands.py
+++ Doctor_Strange/commands.py
@@ -242,9 +242,9 @@ async def start(client, message):
     await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
-        caption=f_caption,
-        reply_markup=InlineKeyboardMarkup(cap_buttons)
+        caption=f_caption,        
         protect_content=True if pre == 'filep' else False,
+        reply_markup=InlineKeyboardMarkup(cap_buttons)
         )
                     
 
