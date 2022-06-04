dc3469fe8cee28dcdf537156268fca4765be66f2
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-04 18:49:40 +0530

    Update pm_filter.py

--- Doctor_Strange/pm_filter.py
+++ Doctor_Strange/pm_filter.py
@@ -174,6 +174,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         title = files.file_name
         size = get_size(files.file_size)
         f_caption = files.caption
+        settings = await get_settings(query.message.chat.id)
         if CUSTOM_FILE_CAPTION:
             try:
                 f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
@@ -187,6 +188,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
         try:
             if AUTH_CHANNEL and not await is_subscribed(client, query):
+                await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+                return
+            elif settings['botpm']:
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                 return            
             else:
