0c27c2092bf7859600af3ae1cf87f98e4675fbf0
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-06-04 17:12:49 +0530

    Update welcome.py

--- Doctor_Strange/welcome.py
+++ Doctor_Strange/welcome.py
@@ -1,13 +1,13 @@
-from pyrogram import filters
+from pyrogram import filters, Client
+from Cluster.sql import setWelcome
 
-
-@app.on_message(filters.command(["setwelcome"]) & filters.group)
+@Client.on_message(filters.command(["setwelcome"]) & filters.group)
 async def setWelcome(client, message):
     if(message.reply_to_message):
-        DB.groups.setWelcome(message.chat.id, message.reply_to_message)
+        setWelcome(message.chat.id, message.reply_to_message)
         await message.reply_text("Successfully set the welcome message")
     else:
         message.text = " ".join(message.command[1:])
-        DB.groups.setWelcome(message.chat.id, message)
+        setWelcome(message.chat.id, message)
         await message.reply_text(f"Successfully set the welcome message to: {' '.join(message.command[1:])}")
 
