c3bba791939385e12523545d84ba87bf7194c319
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-04 18:37:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -103,6 +103,9 @@ MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
+PM_TTI_SHOW_OFF = is_enabled((environ.get('PM_TTI_SHOW_OFF', "False")), False)
+PM_SPELL_CHECK_REPLY = is_enabled(environ.get("PM_SPELL_CHECK_REPLY", "False"), False)
+
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
 LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
