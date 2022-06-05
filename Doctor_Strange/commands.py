9882c9a3bb8e8a7087f7da008345e33f6c0f444d
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-05 16:09:27 +0530

    Update commands.py

--- Doctor_Strange/commands.py
+++ Doctor_Strange/commands.py
@@ -236,18 +236,17 @@ async def start(client, message):
             logger.exception(e)
             f_caption=f_caption
     if f_caption is None:
-        f_caption = f"{files.file_name}"        
+        f_caption = f"{files.file_name}",
+        invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))        
     await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,        
         protect_content=True if pre == 'filep' else False,
-        reply_markup=BTN
+        reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton('🚀 𝙾𝚄𝚁 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 🚀', url=invite_link.invite_link) ]])                            
         )
-    invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
-    BTN = InlineKeyboardMarkup([[ InlineKeyboardButton('🚀 𝙾𝚄𝚁 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 🚀', url=invite_link.invite_link) ]])        
-                    
-
+    
+    
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
            
