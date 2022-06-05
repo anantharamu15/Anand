a884a355cd8ae98982b59c908f96b23aa739f001
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-05 09:33:45 +0530

    Update p_ttishow.py

--- Doctor_Strange/p_ttishow.py
+++ Doctor_Strange/p_ttishow.py
@@ -54,7 +54,7 @@ async def save_group(bot, message):
                         await (temp.MELCOW['welcome']).delete()
                     except:
                         pass
-                temp.MELCOW['welcome'] = await message.reply(WELCOME.format(u.mention, message.chat.title))
+                temp.MELCOW['welcome'] = await message.reply(WELCOME.format(user = u.mention, chat = message.chat.title))
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
