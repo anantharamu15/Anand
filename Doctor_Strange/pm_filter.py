b4c29dd9d6aa3069d2589fe2daf664d7e62949d3
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-05 22:35:09 +0530

    Update pm_filter.py

--- Doctor_Strange/pm_filter.py
+++ Doctor_Strange/pm_filter.py
@@ -277,7 +277,13 @@ async def auto_filter(client, msg, spoll=False):
         if 2 < len(message.text) < 100:
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
-            if not files:                       
+            if not files:
+                if settings["spell_check"]:
+                    return await advantage_spell_chok(msg)
+                else:
+                    return
+        else:
+            return                      
     else:
         settings = await get_settings(msg.message.chat.id)
         message = msg.message.reply_to_message  # msg will be callback query
