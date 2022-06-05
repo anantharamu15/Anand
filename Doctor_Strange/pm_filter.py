a456218438348f334b74ba30a836fd43392eebb0
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-05 17:51:16 +0530

    Update pm_filter.py

--- Doctor_Strange/pm_filter.py
+++ Doctor_Strange/pm_filter.py
@@ -241,10 +241,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         try:
             if AUTH_CHANNEL and not await is_subscribed(client, query):
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
-                return
-            elif settings['botpm']:
-                await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
-                return
+                return            
             else:
                 await client.send_cached_media(
                     chat_id=query.from_user.id,
