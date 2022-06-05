13eb949f339b8966047140851b5aca2c059fb7e5
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-05 18:10:19 +0530

    Update pm_filter.py

--- Doctor_Strange/pm_filter.py
+++ Doctor_Strange/pm_filter.py
@@ -293,7 +293,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"[{get_size(pfile.file_size)}] {pfile.file_name}", callback_data=f'{pre}#{pfile.file_id}'
                 ),
             ]
             for pfile in files
