06d2c134a83ef7607496f3ebd96d60e2a9a5dcb7
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-06 07:51:04 +0530

    Update pm_filter.py

--- Doctor_Strange/pm_filter.py
+++ Doctor_Strange/pm_filter.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 from Doctor_Strange.group_filter import auto_filter
 
 
-@Client.on_message(filters.private & filters.text & filters.incoming)
-async def pm_give_filter(client,message):
-    await auto_filter(client, message)  
+#@Client.on_message(filters.private & filters.text & filters.incoming)
+#async def pm_give_filter(client,message):
+   # await auto_filter(client, message)  
 
