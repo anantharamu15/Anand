173cfd971b12a39c8b3e4f4f7f9e81d24c83fc12
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-06 07:47:57 +0530

    Update new.py

--- Doctor_Strange/new.py
+++ Doctor_Strange/new.py
@@ -6,15 +6,15 @@ import ast
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
-from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
+from Cluster.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
 from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB, SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp
-from database.users_chats_db import db
-from database.ia_filterdb import Media, get_file_details, get_search_results
-from database.filters_mdb import(
+from Cluster.users_chats_db import db
+from Cluster.ia_filterdb import Media, get_file_details, get_search_results
+from Cluster.filters_mdb import(
    del_all,
    find_filter,
    get_filters,
@@ -28,7 +28,7 @@ SPELL_CHECK = {}
 
 
 
-@Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
+@Client.on_message(filters.private & filters.text & ~filters.edited & filters.incoming)
 async def give_filter(client,message):
     k = await manual_filters(client, message)
     if k == False:
