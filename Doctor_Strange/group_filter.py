10dc09cbd3e7d2f8a0ad3a32b3a970cb513fd620
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-06-06 23:22:02 +0530

    Update group_filter.py

--- Doctor_Strange/group_filter.py
+++ Doctor_Strange/group_filter.py
@@ -476,7 +476,46 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
         else:
             return await query.answer(f"😁 𝙷𝙴𝙻𝙻𝙾, {query.from_user.first_name}! {BTN_LOCK_TEXT}", show_alert=True)
-           
+        
+    elif query.data.startswith("file"):
+        ident, file_id = query.data.split("#")
+        files_ = await get_file_details(file_id)
+        if not files_:
+            return await query.answer('No such file exist.')
+        files = files_[0]
+        title = files.file_name
+        size=get_size(files.file_size)
+        f_caption=files.caption
+        if CUSTOM_FILE_CAPTION:
+            try:
+                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+            except Exception as e:
+                logger.exception(e)
+            f_caption=f_caption
+        if f_caption is None:
+            f_caption = f"{files.file_name}"
+            
+        try:
+            if AUTH_CHANNEL and not await is_subscribed(client, query):
+                await query.answer(url=f"https://t.me/{temp.U_NAME}?start={file_id}")
+                return
+            elif P_TTI_SHOW_OFF:
+                await query.answer(url=f"https://t.me/{temp.U_NAME}?start={file_id}")
+                return
+            else:
+                await client.send_cached_media(
+                    chat_id=query.from_user.id,
+                    file_id=file_id,
+                    caption=f_caption
+                    )
+                await query.answer('Check PM, I have sent files in pm',show_alert = True)
+        except UserIsBlocked:
+            await query.answer('Unblock the bot mahn !',show_alert = True)
+        except PeerIdInvalid:
+            await query.answer(url=f"https://t.me/{temp.U_NAME}?start={file_id}")
+        except Exception as e:
+            await query.answer(url=f"https://t.me/{temp.U_NAME}?start={file_id}")   
+            
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
             await query.answer(f"Hey, {query.from_user.first_name}! I Like Your Smartness, But Don't Be Oversmart 😒",show_alert=True)
